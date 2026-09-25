d3ec349348d57eecf8ee73a24ce9c22e8e868646
mahfuz0807 <allwork0807@gmail.com>
2026-09-25 20:50:20 +0600

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/test", (req, res) => {
-  res.json({ ok: true, message: "Backend connected successfully" });
+  res.json({
+    ok: true,
+    message: "Backend connected successfully"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
