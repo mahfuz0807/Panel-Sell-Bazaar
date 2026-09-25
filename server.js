72109ccf040eefc04b0131ffce2a0d7a51548c95
mahfuz0807 <allwork0807@gmail.com>
2026-09-25 21:06:47 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-  res.send("Panel Sell Bazaar backend is running");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/api/test", (req, res) => {
